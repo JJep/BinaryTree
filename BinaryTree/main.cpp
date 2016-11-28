5dae4a2b7ea691fa8637a67c94cf9c86b781ee4c
Jep <xjn377969949@live.com>
2016-11-28 23:13:23 +0800

    BinaryTree 非递归

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -10,11 +10,10 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ElemType char
-
 typedef struct binaryTreeNode {
     char data;
-    struct binaryTreeNode *leftChild,*rightChild;
+    int leftFlag = 0,rightFlag = 0;
+    struct binaryTreeNode *leftChild,*rightChild,*father;
 } binaryTreeNode, *binaryTree;
 
 //先序建立二叉树
@@ -33,6 +32,24 @@ binaryTree createBinaryTree() {
     return T;
 }
 
+//非递归建立二叉树
+binaryTree noRecursionCreateBinaryTree (binaryTree Tree) {
+    char data;
+    std::cin >> data;
+    binaryTree T;
+    if (data == '#') {
+        T = NULL;
+    } else {
+        T = (binaryTree)malloc(sizeof(binaryTreeNode));
+        T -> data = data;
+        T -> leftChild -> father = Tree;
+        T -> leftChild = noRecursionCreateBinaryTree(T);
+        T -> rightChild -> father = Tree;
+        T -> rightChild = noRecursionCreateBinaryTree(T);
+    }
+    return T;
+}
+//MARK: 递归方法遍历二叉树
 //先序遍历二叉树
 void preOrderTraverse(binaryTree T){
     if(T){
@@ -52,6 +69,7 @@ void inOrderTraverse(binaryTree T){
     }
 }
 
+//后序遍历二叉树
 void postOrderTraverse (binaryTree T) {
     if (T) {
         postOrderTraverse(T -> leftChild);
@@ -61,12 +79,39 @@ void postOrderTraverse (binaryTree T) {
     }
 }
 
+//MARK: 非递归方法遍历二叉树
+
+
+void noRecursionPreOrderTraverse (binaryTree T){
+    while (T != NULL) {
+        std::cout << T -> data;
+        if (T -> leftChild != NULL && T -> leftFlag == 1) {
+            T -> leftFlag = 1;
+            T = T -> leftChild;
+        } else if ( T -> rightChild != NULL && T -> rightFlag == 1){
+            T -> rightFlag = 1;
+            T = T -> rightChild;
+        } else {
+            T = T -> father;
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     binaryTree tree;
-    tree = createBinaryTree();
+//    tree = createBinaryTree(); //非递归创建二叉树
+    tree = (binaryTree)malloc(sizeof(binaryTreeNode));
+    char data;
+    std::cin >> data;
+    if (data != '#') {
+        tree -> data = data;
+        tree -> leftChild = noRecursionCreateBinaryTree(tree);
+        tree -> rightChild = noRecursionCreateBinaryTree(tree);
+    }
     std::cout << "Hello, World!\n";
     preOrderTraverse(tree);
+    noRecursionPreOrderTraverse(tree);
     inOrderTraverse(tree);
     postOrderTraverse(tree);
     return 0;
